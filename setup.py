0f783f70e8ffcf2a65a0f41b691ac3f8135e82bc
Mike Davis <mike@optimizely.com>
2013-04-12 14:33:43 -0700

    Remove tests from setup which is causing the build to fail.

--- setup.py
+++ setup.py
@@ -13,14 +13,4 @@ setup(name='mockredispy',
       description='Mock for redis-py',
       url='http://www.github.com/locationlabs/mockredis',
       license='Apache2',
-      packages=find_packages(exclude=['*.tests']),
-      setup_requires=[
-          'nose==1.2.1'
-      ],
-      install_requires=[
-      ],
-      tests_require=[
-          'redis>=2.7.2'
-      ],
-      test_suite='mockredis.tests',
-      )
+      packages=find_packages(exclude=['tests']))
